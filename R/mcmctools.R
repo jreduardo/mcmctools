ec7be7f704d277bea681e2406205018358f392ee
Eduardo Junior <edujrrib@gmail.com>
2018-05-10 23:13:49 -0300

    Add help for the package and add coda on depends

---
+++ R/mcmctools.R
@@ -0,0 +1,8 @@
+#' @name mcmctools
+#' @title Useful Tools to Manipulate MCMC Objects
+#' @docType package
+#' @description Implements functions to manipulate MCMC objects (apply
+#'     thinning, burn-in, select variables, merge chains, etc.) in the
+#'     context of Bayesian inference.
+#' @import coda
+NULL
